2d553353fd05088f79e37ad130ba97fd0f15ea17
Franz <franz.benthin.dev@gmail.com>
2026-03-04 07:20:12 +0100

    feat: add public API entry point

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { useMagnetic } from "./use-magnetic.js";
+export { Magnetic } from "./magnetic.js";
+export type { MagneticOptions, MagneticState } from "./types.js";
+export type { MagneticProps } from "./magnetic.js";
